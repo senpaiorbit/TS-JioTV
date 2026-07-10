e0aa95704cfbc50011b06c9db429d4a174dd8ae5
tanbirst1st1 <senpaiorbit@gmail.com>
2026-07-10 11:36:33 +0530

    Remove logout.js (merged into refresh.js)

--- vercel/api/logout.js
+++
@@ -1,9 +0,0 @@
-const { deleteCredentialsCookies, setCorsHeaders } = require('./lib/functions');
-
-module.exports = async function handler(req, res) {
-  setCorsHeaders(res);
-  if (req.method === 'OPTIONS') return res.status(200).end();
-
-  deleteCredentialsCookies(res);
-  return res.status(200).json({ status: 'success', message: 'Logged out' });
-};
